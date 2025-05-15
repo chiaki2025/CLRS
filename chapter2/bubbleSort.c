cbfac7a97c3edf858337aa07c303e463b245a625
chiaki2025 <tinvu13092003@gmail.com>
2025-05-16 01:12:57 +0700

    sorting algorithms

--- chapter2/bubbleSort.c
+++ chapter2/bubbleSort.c
@@ -2,8 +2,8 @@
 int main(){
     int arr[] = {1, 4, 2, 0, -23, 34, -100};
     int n = sizeof(arr)/sizeof(arr[0]);
-    for(int i = 0; i < n - 1; i++){
-        for(int j = 0; j < n - i - 1; j++){
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n - i; j++){
             if(arr[j] > arr[j + 1]){
                 int temp = arr[j + 1];
                 arr[j + 1] = arr[j];
